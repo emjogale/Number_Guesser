db48f5af76ad7ee1c28b8c5692c77845a898e6b8
emjogal <emmajgale@hotmail.com>
2019-06-24 08:33:08 +0100

    sorting out logic of game

--- script.js
+++ script.js
@@ -3,4 +3,26 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+let generateTarget = function() {
+  return Math.floor(Math.random()*9);
+};
 
+let compareGuesses = function(humanScore, computerScore, targetNumber) {
+  if (Math.abs(targetNumber - humanScore) <= Math.abs(targetNumber - computerScore)) {
+    return true;
+    } else {
+    return false;
+  }
+};
+
+let updateScore = function(winner) {
+  if (winner === 'human'){
+    humanScore += 1;
+  } else if(winner === 'computer'){
+    computerScore += 1;
+  }
+};
+
+let advanceRound = function() {
+  currentRoundNumber ++;
+}
